541cd737c1d9d2605e022486953890c15d5bbc87
srenidhin <srenidhin@gmail.com>
2021-11-27 14:53:31 +0530

    Add packages

--- app/views.py
+++ app/views.py
@@ -6,7 +6,6 @@ from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpRequest
 from django.http import HttpResponse 
-import pickle 
 import numpy as np
 import pandas as pd
 from sklearn import model_selection
